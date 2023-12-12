e8a7bf751574a64c189c5cee90ef474f734254bf
karollg123 <153654917+karollg123@users.noreply.github.com>
2023-12-12 11:57:14 -0500

    Add files via upload

---
+++ maincv.py
@@ -0,0 +1,39 @@
+# Suma
+nm1 = 19
+nm2 = 25
+suma = nm1 + nm2
+print("La suma de", nm1, "y", nm2, "es:", suma)
+# Resta
+nm3 = 15.5
+nm4 = 7.5
+resta = nm3 - nm4
+print("La resta de", nm3, "y", nm4, "es:", resta)
+# Multiplicacion
+nm5 = 12
+nm6 = 15
+mult = nm5 * nm6
+print("La multiplicación de", nm5, "y", nm6, "es:", mult)
+# Divide
+nm7 = 30.5
+nm8 = 5.5
+division = nm7 / nm8
+print("La división de", nm7, "y", nm8, "es:", division)
+# Nombre completo
+nombres= "carolina estefanny zhirzhan lopez"
+# Longitud del nombre completo
+long= len(nombres)
+# Imprime en pantalla
+print(nombres)
+print(long)
+#Declara una variable entera y otra de punto flotante
+a=25
+b=19.1
+#Realiza la conversión de la variable entera a punto flotante y viceversa
+float(a)
+int(b)
+#Imprime en pantalla el tipo de dato original y el tipo de dato después de la conversión
+print(a)
+print(b)
+#dato después de la conversión
+print(float(a))
+print(float(b))
